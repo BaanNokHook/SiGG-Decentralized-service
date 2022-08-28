f3bee1b9d490ca364ab838eebcc013dc2690115a
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-08-28 18:26:17 +0700

    Wallet/src/components//CredentialItemPresentation/CredentialItemPresentation/test

---
+++ Wallet/src/components/CredentialItemPresentation/CredentialItemPresentation.test.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { render } from "@testing-library/react";
+import CredentialItemPresentation from "./CredentialItemPresentation";
+import * as mocks from "../../test/mocks/mocks";
+
+
+describe("credential item presentation", () => {  
+
+   it("should render without crashong", () => {  
+      expect.assertions(1);  
+      const credential = mocks.getCredential; 
+      const methodToOpen = jest.fn();    
+      const wrapper = render(  
+         <BrowserRouter>
+          <CredentialItemPresentation
+            credential={credential}  
+            methodToSelect={methodToOpen}  
+            defaultChecked={false}  
+           />  
+         </BrowserRouter>
+      );  
+      expect(wrapper).toBeDefined();  
+   });
+});  
+
