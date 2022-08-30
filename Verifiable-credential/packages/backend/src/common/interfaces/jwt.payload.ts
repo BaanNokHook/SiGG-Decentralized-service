3e4899b13644613c435692dcb1b6deebd6276afc
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-08-30 16:54:53 +0700

    Verifiable-credential/packages/backend/src/common/interfaces/jwt/payload

---
+++ Verifiable-credential/packages/backend/src/common/interfaces/jwt.payload.ts
@@ -0,0 +1,10 @@
+export interface JwtPayload {
+      iat?: number;
+      exp?: number;
+      sub?: string;
+      aud?: string;
+      did?: string;
+      nonce?: string;
+      iss?: string;
+    }
+    
