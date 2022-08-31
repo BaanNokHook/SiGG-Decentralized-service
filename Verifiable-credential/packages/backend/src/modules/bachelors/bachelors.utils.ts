5fd417c63f5e44da3e87576c6fb7b452a83c5f13
chinp443@entro.postbox.in.th <chinp443@entro.postbox.in.th>
2022-08-31 16:06:11 +0700

    Verifiable-credential/packages/backend/src/modules/bachelors/bachelors/utils

---
+++ Verifiable-credential/packages/backend/src/modules/bachelors/bachelors.utils.ts
@@ -0,0 +1,65 @@
+import { decodeToken } from "../../common/utils/jwt.utils";
+import { FullVerifiableAttestation } from "./interfaces/full-verifiable-attestation";
+import { SignedVc } from "./interfaces/signed-vc";
+import {
+  DEFAULT_EIDAS_PROOF_TYPE,
+  DEFAULT_PROOF_PURPOSE,
+  DEFAULT_EIDAS_VERIFICATION_METHOD,
+} from "./constants";
+import bachelorsDiplomaStructure from "./bachelor-diploma-structure.json";
+
+export function formatVerifiableAttestation(raw, issuerDid) {
+  if (!raw || !issuerDid) {
+    throw new Error("Missing parameters in formatVerifiableId function");
+  }
+
+  const verifiableId = {
+    ...bachelorsDiplomaStructure,
+  };
+
+  verifiableId.credentialSubject.id = raw.did;
+  verifiableId.issuer.id = issuerDid;
+
+  const verifiableIdCredentialPayload = {
+    sub: raw.did,
+    vc: {
+      "@context": [
+        "https://www.w3.org/2018/credentials/v1",
+        "https://SiGG-WEBSITE.TH/schemas/vc/2019/v1#",
+        "https://SiGG-WEBSITE.TH/schemas/eidas/2019/v1#",
+      ],
+      id: verifiableId.id,
+      type: verifiableId.type,
+      issuer: verifiableId.issuer,
+      credentialSubject: verifiableId.credentialSubject,
+    },
+  };
+
+  return verifiableIdCredentialPayload;
+}
+
+export const createFullVerifiableAttestation = (vcJwt: string) => {
+  const decodedVc = <SignedVc>decodeToken(vcJwt);
+  const { iat, iss, vc } = decodedVc;
+
+  if (!iat) throw Error("Error processing JWT iat");
+
+  const issuanceDate = new Date(iat * 1000).toISOString();
+
+  return <FullVerifiableAttestation>{
+    "@context": vc["@context"],
+    id: vc.id,
+    type: vc.type,
+    issuer: iss,
+    issuanceDate,
+    // expirationDate: moment().add(10, "years").toISOString(),
+    credentialSubject: vc.credentialSubject,
+    proof: {
+      type: DEFAULT_EIDAS_PROOF_TYPE,
+      created: issuanceDate,
+      proofPurpose: DEFAULT_PROOF_PURPOSE,
+      verificationMethod: iss + DEFAULT_EIDAS_VERIFICATION_METHOD,
+      jws: vcJwt,
+    },
+  };
+};
