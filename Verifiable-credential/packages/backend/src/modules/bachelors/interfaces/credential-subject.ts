0e186be325b96ce0fc7a001ac67d3732b554ffff
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-08-31 09:41:08 +0700

    Verifiable-credential/packages/backend/src/modules/bachelors/interfaces/credential-subject

---
+++ Verifiable-credential/packages/backend/src/modules/bachelors/interfaces/credential-subject.ts
@@ -0,0 +1,14 @@
+export interface CredentialSubject {  
+      id: string;  
+      personalIdentifier: string;  
+      currentFamilyName: string; 
+      currentGivenName: string;  
+      birthName: string; 
+      dateOfBirth: string; 
+      placeOfBirth: string;
+      currentAddress: string;  
+      gender: string;  
+      govID?: string
+}
+
+
