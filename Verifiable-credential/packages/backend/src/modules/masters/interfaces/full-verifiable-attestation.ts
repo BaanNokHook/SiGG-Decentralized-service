94a02fb03f8127da2e786a8a7a91ce8bb3eb0934
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-09-01 16:58:27 +0700

    Verifiable-credential/packages/backend/src/modules/masters/interfaces/full-verifiable-attestation

---
+++ Verifiable-credential/packages/backend/src/modules/masters/interfaces/full-verifiable-attestation.ts
@@ -0,0 +1,9 @@
+import { VerifiableAttestation } from "./verifiable-attestation";
+import { Proof } from "./proof";
+
+export interface FullVerifiableAttestation extends VerifiableAttestation {
+  issuer: string;
+  issuanceDate: string;
+  proof: Proof;
+  expirationDate: string;
+}
